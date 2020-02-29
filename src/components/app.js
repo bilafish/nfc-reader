ef11a6a60024edfd046d3818cd970159a4a24251
Jason <bilafish@gmail.com>
2020-03-01 06:39:27 +0800

    :wrench: Update config for env variables

--- src/components/app.js
+++ src/components/app.js
@@ -16,12 +16,16 @@ export default class App extends Component {
   handleRoute = e => {
     this.currentUrl = e.url;
   };
+  // <meta http-equiv="origin-trial" content={process.env.ORIGIN_TOKEN} />
 
   render() {
     return (
       <div id="app">
         <Helmet>
-          <meta http-equiv="origin-trial" content={process.env.ORIGIN_TOKEN} />
+          <meta
+            http-equiv="origin-trial"
+            content={process.env.PREACT_APP_ORIGIN_TOKEN}
+          />
         </Helmet>
         <Header />
         <Router onChange={this.handleRoute}>
