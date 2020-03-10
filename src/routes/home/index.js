579c4e546391b7c3354fdfb847f46a48688cdba0
Jason <bilafish@gmail.com>
2020-03-10 16:22:26 +0800

    :sparkles: Add NFC reading feature
    - Add button to start NFC scan

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,10 +1,27 @@
 import { h } from "preact";
+import { useState } from "preact/hooks";
 import style from "./style";
+import styled from "styled-components";
 import ScanLottie from "../../components/scanlottie";
 import SwitchSelector from "react-switch-selector";
 import Alert from "../../components/alert";
+import NfcConsole from "../../components/nfc-console";
+
+const ScanButton = styled.button`
+  background: #ffc86b;
+  color: black;
+  border: none;
+  padding: 12px 40px;
+  border-radius: 10px;
+  font-size: 16px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
 
 const Home = () => {
+  const [feed, setFeed] = useState("");
+  const [buttonText, setButtonText] = useState("Start Scan");
+
   const options = [
     {
       label: "Read",
@@ -26,6 +43,34 @@ const Home = () => {
     ({ value }) => value === "read"
   );
 
+  const nfcScan = async () => {
+    try {
+      const reader = new NDEFReader();
+      await reader.scan();
+      setButtonText("Scanning...");
+
+      reader.addEventListener("error", () => {
+        setFeed(feed => [
+          ...feed,
+          "Argh! Cannot read data from the NFC tag. Try a different one?"
+        ]);
+      });
+
+      reader.addEventListener("reading", ({ message, serialNumber }) => {
+        setFeed(`${serialNumber}`);
+        // if (message.records.length > 0) {
+        //   setFeed(feed => [...feed, `> Message: ${message.records[0]}`]);
+        // }
+      });
+    } catch (error) {
+      setFeed(feed => [...feed, `Argh! ${error}`]);
+    }
+  };
+
+  const scanButtonHandler = () => {
+    nfcScan();
+  };
+
   return (
     <div class={style.home}>
       <Alert />
@@ -40,6 +85,8 @@ const Home = () => {
         />
       </div>
       <ScanLottie />
+      <ScanButton onClick={scanButtonHandler}>{buttonText}</ScanButton>
+      <NfcConsole feed={feed} />
     </div>
   );
 };
